5b9c5729d92463c7e69395bf03116316f4e43c7c
Ahany42 <ahanyfathy42@gmail.com>
2025-01-22 00:53:32 +0200

    version 0.1.0

--- extension.js
+++ extension.js
@@ -1,43 +1,56 @@
 const vscode = require('vscode');
 let idleTimer = 0;
 let isIdle = false;
-const idleTimeLimit = 5 * 60 * 1000; 
+const idleTimeLimit = 5 * 60 * 1000;
+const idleCheckInterval = 60 * 1000;
 let lastActiveTime = Date.now();
-/**
- * @param {vscode.ExtensionContext} context
- */
-const resetIdleTimer = () =>{
-lastActiveTime = Date.now();
-if(isIdle){
-	isIdle = false;
-	vscode.window.showInformationMessage("Welcome back! Idle period ended.");
+let updateIdleTimer = 0;
+
+const resetIdleTimer = () => {
+  lastActiveTime = Date.now();
+  if (isIdle) {
+    isIdle = false;
+    vscode.window.showInformationMessage("Welcome back! Idle period ended.");
+  }
+  clearTimeout(idleTimer);
+  clearInterval(updateIdleTimer);  
+  idleTimer = setTimeout(setIdle, idleTimeLimit);
+  updateIdleTimer = setInterval(updateIdleStatus, idleCheckInterval); 
 }
-clearTimeout(idleTimer);
-idleTimer = setTimeout(setIdle, idleTimeLimit);
+
+const setIdle = () => {
+  isIdle = true;
+  updateIdleStatus();  
 }
-const setIdle = ()=>{
-	isIdle = true;
-	const idleDuration = Math.round((Date.now() - lastActiveTime)/60000);
-	vscode.window.showInformationMessage(`You have been idle for ${idleDuration} mins`);
+
+const updateIdleStatus = () => {
+  if (isIdle) {
+    const idleDuration = Math.round((Date.now() - lastActiveTime) / 60000); 
+    vscode.window.showInformationMessage(`You have been idle for ${idleDuration} mins`);
+  }
 }
+
 function activate(context) {
-	const disposable = vscode.commands.registerCommand('idle-time-detector.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from idle time detector!');
-	});
-	context.subscriptions.push(
-        vscode.workspace.onDidChangeTextDocument(() => resetIdleTimer()),
-        vscode.window.onDidChangeActiveTextEditor(() => resetIdleTimer()),
-        vscode.window.onDidChangeVisibleTextEditors(() => resetIdleTimer())
-    );
-	context.subscriptions.push(disposable);
-	resetIdleTimer();
+  const disposable = vscode.commands.registerCommand('idle-time-detector.helloWorld', function () {
+    vscode.window.showInformationMessage('Hello World from idle time detector!');
+  });
+
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument(() => resetIdleTimer()),
+    vscode.window.onDidChangeActiveTextEditor(() => resetIdleTimer()),
+    vscode.window.onDidChangeVisibleTextEditors(() => resetIdleTimer())
+  );
+  
+  context.subscriptions.push(disposable);
+  resetIdleTimer();  
 }
 
 function deactivate() {
-	clearTimeout(idleTimer)
+  clearTimeout(idleTimer);
+  clearInterval(updateIdleTimer); 
 }
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
 }
